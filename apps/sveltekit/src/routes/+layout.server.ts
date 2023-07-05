c1c5354aad3344fd9c27d960e60eeab1df61eab3
Fractal-Tess <vgfractal@gmail.com>
2023-07-06 01:53:12 +0300

    settings

--- apps/sveltekit/src/routes/+layout.server.ts
+++ apps/sveltekit/src/routes/+layout.server.ts
@@ -1,5 +1,8 @@
 import type { LayoutServerLoad } from './$types';
 
+export const ssr = false;
+export const prerender = true;
+
 export const load = (({ locals: { theme } }) => {
   return {
     theme
