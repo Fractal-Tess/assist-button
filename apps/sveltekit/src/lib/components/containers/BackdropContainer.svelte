627703b79d8b7e986182d1f3b67d8815da4919e5
Fractal-Tess <vgfractal@gmail.com>
2023-07-06 01:53:06 +0300

    format

--- apps/sveltekit/src/lib/components/containers/BackdropContainer.svelte
+++ apps/sveltekit/src/lib/components/containers/BackdropContainer.svelte
@@ -1,3 +1,3 @@
-<div class="rounded-md bg-base-100 p-8">
+<div class="bg-base-100 rounded-md p-8">
   <slot />
 </div>
--- apps/sveltekit/src/lib/components/headers/BaseHeader.svelte
+++ apps/sveltekit/src/lib/components/headers/BaseHeader.svelte
@@ -6,7 +6,7 @@
 
 <div class="flex h-20 items-center justify-end px-4">
   <div
-    class="flex items-center space-x-4 [&>*:hover]:text-primary [&>*]:transition-all [&>*]:duration-300">
+    class="[&>*:hover]:text-primary flex items-center space-x-4 [&>*]:transition-all [&>*]:duration-300">
     <a
       href="https://github.com/Fractal-Tess/SvelteKit"
       rel="noreferrer"
