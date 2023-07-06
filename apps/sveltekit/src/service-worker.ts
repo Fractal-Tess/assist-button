4ccb731e61b4d02501a1892adef5ac1a1485a0b6
Fractal-Tess <vgfractal@gmail.com>
2023-07-06 22:35:38 +0300

    update service-worker

--- apps/sveltekit/src/service-worker.ts
+++ apps/sveltekit/src/service-worker.ts
@@ -1,4 +1,11 @@
 /// <reference types="@sveltejs/kit" />
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
+
+// https://kit.svelte.dev/docs/service-workers#type-safety
+const sw = self as unknown as ServiceWorkerGlobalScope;
+
 import { build, files, version } from '$service-worker';
 
 // Create a unique cache name for this deployment
@@ -9,38 +16,54 @@ const ASSETS = [
   ...files // everything in `static`
 ];
 
-self.addEventListener('install', event => {
+sw.addEventListener('install', event => {
   // Create a new cache and add all files to it
-  async function addFilesToCache() {
+  async function addFilesToCacheAndSkipWaiting() {
     const cache = await caches.open(CACHE);
     await cache.addAll(ASSETS);
+    await sw.skipWaiting();
   }
 
-  event.waitUntil(addFilesToCache());
+  event.waitUntil(addFilesToCacheAndSkipWaiting());
 });
 
-self.addEventListener('activate', event => {
+sw.addEventListener('activate', event => {
   // Remove previous cached data from disk
-  async function deleteOldCaches() {
+  async function deleteOldCachesAndClaimClients() {
     for (const key of await caches.keys()) {
       if (key !== CACHE) await caches.delete(key);
     }
+
+    await sw.clients.claim();
   }
 
-  event.waitUntil(deleteOldCaches());
+  event.waitUntil(deleteOldCachesAndClaimClients());
 });
 
-self.addEventListener('fetch', event => {
-  // ignore POST requests etc
+sw.addEventListener('fetch', event => {
+  // Ignore requests that should be cached
+  const matchUrl = new URL(event.request.url);
   if (event.request.method !== 'GET') return;
+  if (matchUrl.pathname.startsWith('/api')) return;
+  if (matchUrl.pathname.startsWith('/admin')) return;
+  if (matchUrl.pathname.startsWith('/dashboard')) return;
+  if (matchUrl.pathname.startsWith('/status')) return;
 
   async function respond() {
     const url = new URL(event.request.url);
     const cache = await caches.open(CACHE);
 
+    // https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
+
     // `build`/`files` can always be served from the cache
-    if (ASSETS.includes(url.pathname)) {
-      return cache.match(url.pathname);
+    // Here we can end up in a crazy state where some of the cache is gone, which
+    // leads us to white screen of death
+    const cacheMatch = await cache.match(event.request);
+
+    // TODO: make issue on Kit github
+    // Work around for if cache has been partly deleted
+    if (ASSETS.includes(url.pathname) && cacheMatch) {
+      return cacheMatch;
     }
 
     // for everything else, try the network first, but
@@ -49,24 +72,92 @@ self.addEventListener('fetch', event => {
       const response = await fetch(event.request);
 
       if (response.status === 200) {
-        cache.put(event.request, response.clone());
+        await cache.put(event.request, response.clone());
       }
 
       return response;
     } catch {
-      return cache.match(event.request);
+      // Insanity is doing the same thing twice and hoping for a different result
+      const lastCacheMatchAttempt = await cache.match(event.request);
+
+      if (lastCacheMatchAttempt) {
+        return lastCacheMatchAttempt;
+      } else {
+        return new Response(
+          'Something went very wrong. Try force closing and reloading the app.',
+          {
+            status: 408,
+            headers: { 'Content-Type': 'text/html' }
+          }
+        );
+      }
     }
   }
 
+  // TODO!: Would be better to omit this(?) if the response is undefined
   event.respondWith(respond());
 });
 
-self.addEventListener('push', event => {
-  const title = 'Button pressed';
-  const options = {
-    icon: 'logo.png',
-    badge: 'logo.png'
-  };
-  console.log('here');
-  event.waitUntil(self.registration.showNotification(title, options));
+sw.addEventListener('push', function (event) {
+  try {
+    const payload = event.data
+      ? event.data.json()
+      : {
+          title: 'Appreciation Jar',
+          body: 'There is new content in your Appreciation Jar!'
+        }; // Basically a fallback message in case something goes wrong
+
+    if (payload) {
+      const { title, ...options } = payload;
+
+      event.waitUntil(sw.registration.showNotification(title, options));
+    } else {
+      console.warn('No payload for push event', event);
+    }
+
+    // TODO: We can also implement analytics for received pushes as well if we want:
+    // https://web.dev/push-notifications-handling-messages/#wait-until
+  } catch (e) {
+    console.warn('Malformed notification', e);
+  }
+});
+
+sw.addEventListener('notificationclick', (event: any) => {
+  const clickedNotification = event?.notification;
+
+  // console.log('CLICKED NOTIFICATION');
+  clickedNotification.close();
+
+  event.waitUntil(
+    sw.clients
+      .matchAll({ type: 'window' })
+      .then(clientsArr => {
+        // console.log('matching sw', clientsArr)
+        /*
+				const hadWindowToFocus = clientsArr.some((windowClient) =>
+					windowClient.url.includes('/jar') ? (windowClient.focus(), true) : false
+				);
+				*/
+
+        // https://web-push-book.gauntface.com/common-notification-patterns/
+
+        // If we have a client, pick the first one and open it
+        const hadWindowToFocus = clientsArr.length && clientsArr.length > 0;
+
+        // Otherwise, open a new tab to the applicable URL and focus it.
+        if (hadWindowToFocus) {
+          const client = clientsArr[0];
+          if (!client.url.includes('/jar')) {
+            client.navigate('/jar');
+          }
+          client.focus();
+        } else
+          sw.clients
+            .openWindow('/jar')
+            .then(windowClient => (windowClient ? windowClient.focus() : null));
+      })
+      .catch(e => {
+        console.error(e);
+      })
+  );
 });
